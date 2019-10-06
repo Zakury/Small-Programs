3e9c0cf4c6eca5c6e9a864a8e86692e957c98589
Zachary <zachary@kaczorzoo.net>
2019-10-05 21:37:00 -0400

    Update tail.py

--- tail.py
+++ tail.py
@@ -1,22 +1,11 @@
 import time
 
 def logger(file_name, interval = 0.1):
-    """
-    A generator that returns any new lines added to a file.
-    """
-
-    # Open the file.
     with open(file_name) as file:
-        # Read any already existing lines.
         file.readlines()
 
-        # Constantly check for new line.
         while True:
-            # Attempt to get a newline, and remove any whitespace.
             new_line = file.readline().strip()
-            
-            # If the attempt is successful, send the new line.
             if new_line: yield new_line
 
-            # Wait some time.
             time.sleep(interval)
